c4172af023abe1fe74c9ad0bf466da47bcd1b1ab
Dhirendra Kathayat <107436829+Dhiren1211@users.noreply.github.com>
2026-05-18 10:59:04 +0900

    Update car.cpp

--- Inheritance/car.cpp
+++ Inheritance/car.cpp
@@ -1,16 +1,20 @@
 #include "car.h"
-car::Car(long nr, string producer) {
+Car::Car(long nr, string producer)
+{
     this->nr = nr;
     this->producer = producer;
 }
-void Car::display() const {
-    cout << "Car Number: " << nr << ", Producer: " << producer << endl;
+void Car::display() const
+{
+    cout << "Car Number: " << nr << "\n Producer: " << producer << endl;
 }
-PassCar::PassCar(long nr, string producer, const string& passCarType, bool sunRoof) : Car(nr, producer) {
+PassCar::PassCar(long nr, string producer, const string &passCarType, bool sunRoof) : Car(nr, producer)
+{
     this->passCarType = passCarType;
     this->sunRoof = sunRoof;
 }
-void PassCar::display() const {
+void PassCar::display() const
+{
     Car::display(); // Call base class display
-    cout << "Passenger Car Type: " << passCarType << ", Sunroof: " << (sunRoof ? "Yes" : "No") << endl;
+    cout << "Passenger Car Type: " << passCarType << "\n Sunroof: " << (sunRoof ? "Yes" : "No") << endl;
 }
