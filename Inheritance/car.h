3e81acc446f479d6076ad59d4b09edce64cfa093
Dhirendra Kathayat <107436829+Dhiren1211@users.noreply.github.com>
2026-05-18 10:59:24 +0900

    Refactor Car and PassCar classes for consistency

--- Inheritance/car.h
+++ Inheritance/car.h
@@ -1,52 +1,46 @@
 #ifndef CAR_H
 #define CAR_H
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 
-class Car {
-    private:
+class Car
+{
+private:
     long nr;
     string producer;
-    public:
-    Car(long nr, string producer) {
-        this->nr = nr;
-        this->producer = producer;
-    }
-    long getNr() {
-        return nr;
-    }
-    string getProducer() {
-        return producer;
-    }
-    void setNr(long nr) {
+
+public:
+    Car(long nr, string producer);
+    long getNr() const { return nr; }
+    string getProducer() const { return producer; }
+    void setNr(long nr)
+    {
         this->nr = nr;
     }
-    void setProducer(string producer) {
+    void setProducer(string producer)
+    {
         this->producer = producer;
     }
     void display() const;
 };
 
-class PassCar : public Car {
-    private:
+class PassCar : public Car
+{
+private:
     string passCarType;
     bool sunRoof;
-    public:
-    PassCar(long nr, string producer, const string& passCarType, bool sunRoof) : Car(nr, producer) {
-        this->passCarType = passCarType;
-        this->sunRoof = sunRoof;
-    }
-   const string& getType() {
-        return passCarType;
-    }
-    bool getSunRoof() {
-        return sunRoof;
-    }
-    void setType(const string& passCarType) {
+
+public:
+    PassCar(long nr, string producer, const string &passCarType, bool sunRoof);
+    const string &getType() const { return passCarType; }
+    bool getSunRoof() const { return sunRoof; }
+    void setType(const string &passCarType)
+    {
         this->passCarType = passCarType;
     }
-    void setSunRoof(bool sunRoof) {
+    void setSunRoof(bool sunRoof)
+    {
         this->sunRoof = sunRoof;
     }
     void display() const;
